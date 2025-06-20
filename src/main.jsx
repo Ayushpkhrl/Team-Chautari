4ffd208dadf33c919c0ac0fd2f9bc7e8883a9918
Ayushpkhrl <pokhrelayush8@gmail.com>
2025-06-21 00:26:09 +0545

    added all the files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { Router, RouterProvider } from 'react-router-dom'
+import Landingpage from './pages/Landingpage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landingpage />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
-)
+);
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import Header from '../components/navbar/Header'
-import Card from '../components/content/Card'
-import Hero from '../components/content/hero'
+import React from 'react';
+import Header from '../components/navbar/Header';
+import Card from '../components/content/Card';
+import Hero from '../components/content/hero';
 
 const Landingpage = () => {
-  const image= 'ayush.jpg'
-  const user="oll"
-  const UserName="Ayush";
-  const city ="Gachhiya";
+  const image = 'ayush.jpg'; // Ensure this is correctly placed
+  const UserName = 'Ayush';
+  const city = 'Gachhiya';
+
   return (
     <div>
-    <Header UserName={UserName} />
-    <Hero/>
-      {/*<Card  UserName={UserName} city={city} image={image}/>*/}
-   
+      <Header UserName={UserName} />
+      <Hero />
+      <Card UserName={UserName} city={city} image={image} />
     </div>
-  )
-}
+  );
+};
 
-export default Landingpage
+export default Landingpage;
